fed84cbde26e5fa0669abfd19a89c85ff11f02c3
Noelryn <noelgerard.ryan@gmail.com>
2021-02-18 16:12:23 +0100

    Creating a user can calculate their bmi file in the Cypress intergration folder, adding tests to the test file and adding a form to the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,17 @@ import React, { Component } from "react"
 
 class App extends Component {
   render() {
-    return <div></div>
+    return (
+      <form>
+        <label htmlFor="weight">Weight</label>
+        <input type="number" required placeholder="Weight in KG" name="weight" id="weight"/>
+
+        <label htmlFor="height">Height</label>
+        <input type="number" required placeholder="Height in CM" name="height" id="height"/>
+
+        <button id="calculate">Calculate</button>
+    </form>
+    )
   }
 }
 
